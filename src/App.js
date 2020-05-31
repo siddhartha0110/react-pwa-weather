c9013d399f8c211920d5865e8f907df6e72a4383
Siddhartha Saxena <siddharthasaxena.1998@gmail.com>
2020-05-31 15:41:07 +0530

    PWA React Weather

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import fetchWeather from "./api/fetchWeather";
+import "./App.css";
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      console.log(data);
+      setWeather(data);
+      setQuery("");
+    }
+  };
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search ..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <p>
+              <b>
+                <i>
+                  Real Feel: {weather.main.feels_like}
+                  <sup>&deg;C</sup>
+                </i>
+              </b>
+            </p>
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+            <p>
+              Latitude: {weather.coord.lat}
+              <br />
+              Longitude: {weather.coord.lon}
+              <br />
+              Humidity: {weather.main.humidity}
+              <br />
+              Wind Speed: {weather.wind.speed} m/s
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
